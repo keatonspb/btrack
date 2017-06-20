3b6171ce817fb7a4bace86b18bd9683a11b00eab
Бычковский Константин <bk@101media.ru>
2017-06-20 16:05:55 +0300

    API

--- backtrack/app/Http/Controllers/ApiController.php
+++ backtrack/app/Http/Controllers/ApiController.php
@@ -17,14 +17,17 @@ class ApiController extends Controller
     public function search(Request $request) {
         $q = $request->get("q");
         $songs = Song::orderBy("created_at", "DESC");
+        $appends = [];
         if($q) {
             $songs->where("songs.name", "like", $q."%")->orWhere('authors.name', "like", $q."%");
+            $appends['q'] = $q;
         }
         $songs->leftJoin('authors', 'songs.author_id', '=', 'authors.id');
         $songs->groupBy("songs.id");
         $songs->select("songs.*", "authors.name as author_name");
         $songs->with("tracks");
-        $json = $songs->paginate(10)->toArray();
+
+        $json = $songs->paginate(10)->appends($appends)->toArray();
         return json_encode($json); 
 
     }
