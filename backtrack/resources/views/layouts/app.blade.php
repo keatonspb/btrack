09f686880871f86a031608eea67bfc06a3111eb9
Бычковский Константин <keatonspb@gmail.com>
2017-08-01 12:21:02 +0300

    Hide login

--- backtrack/resources/views/layouts/app.blade.php
+++ backtrack/resources/views/layouts/app.blade.php
@@ -67,7 +67,7 @@
                     <ul class="nav navbar-nav navbar-right">
                         <!-- Authentication Links -->
                         @if (Auth::guest())
-                            <li><a href="{{ route('login') }}">{{__('messages.add_backing_track')}}</a></li>
+                            {{--<li><a href="{{ route('login') }}">{{__('messages.add_backing_track')}}</a></li>--}}
                         @else
                             <li class="dropdown">
                                 <a href="#" class="dropdown-toggle" data-toggle="dropdown" role="button" aria-expanded="false">
